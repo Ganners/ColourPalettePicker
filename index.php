2013171e4f62ce69dd6f841d3e151d4521e68a0c
Mark Gannaway <mark@ganners.co.uk>
2013-03-06 22:44:47 +0000

    Continued work on prototype. Now produces comparison tables, frequency tables and can loop to discard similar colours from frequency

--- index.php
+++ index.php
@@ -14,14 +14,12 @@ class RGB {
 
 	/**
 	 * Creates the RGB object
-	 * @param int $r - Red value
-	 * @param int $g - Green value
-	 * @param int $b - Blue value
+	 * @param int $colourIndex - RGB integer value of pixel
 	 */
-	public function __construct($r, $g, $b) {
-		$this->red = $r;
-		$this->green = $g;
-		$this->blue = $b;
+	public function __construct($colourIndex) {
+		$this->red = ($colourIndex >> 16) & 0xFF;
+		$this->green = ($colourIndex >> 8) & 0xFF;
+		$this->blue = $colourIndex & 0xFF;
 	}
 
 	/**
@@ -65,13 +63,31 @@ class RGB {
 	}
 
 	/**
+	 * 
+	 */
+	public function getDistance($colour) {
+		if(is_int($colour)) {
+			$colour = new RGB($colour);
+		}
+
+		$distance = sqrt(
+				pow($colour->getRed()-$this->getRed(), 2) + 
+				pow($colour->getGreen()-$this->getGreen(), 2) +
+				pow($colour->getBlue()-$this->getBlue(), 2)
+			);
+
+
+		return $distance;
+	}
+
+	/**
 	 * Converts RGB to Hex string
 	 * @return string
 	 */
 	public function getHex() {
 		$red = str_pad(dechex($this->red), 2, '0');
 		$green = str_pad(dechex($this->green), 2, '0');
-		$blue =str_pad(dechex($this->blue), 2, '0');
+		$blue = str_pad(dechex($this->blue), 2, '0');
 		return "#{$red}{$green}{$blue}";
 	}
 
@@ -86,23 +102,27 @@ class Create_Colour_Scheme {
 			/**
 			 * The image resouces, from GD lib
 			 */
-    private $imageResource,
+    private $_imageResource,
 			/**
 			 * The colour scheme containing final colours
 			 */
-    		$colourScheme = array(),
+    		$_colourScheme = array(),
 			/**
 			 * The colour table - probably a table of all colours
 			 * and their occurance
 			 */
-    		$colourTable = array();
+    		$_colourTable = array();
 
     /**
      * Sets up the object
      * @param resource $image_resource - Our GDLib image resource
      */
     public function __construct($image_resource) {
-        $this->imageResource = $image_resource;
+        $this->_imageResource = $image_resource;
+        $this->_colourTable = $this->_createColourTable();
+        $this->_frequencyTable = $this->_createFrequencyTable($this->_colourTable);
+        $this->_frequencyTable = $this->_applyFrequencyWeights($this->_frequencyTable);
+        var_dump($this->_frequencyTable);
     }
 
     /**
@@ -110,9 +130,96 @@ class Create_Colour_Scheme {
      * @param $return_type - What format the colours should be returned (rgb|hex)
      * @return array
      */
-    private function createColourTable($return_type = 'rgb') {
-    	
+    private function _createColourTable() {
+    	$colourTable = array();
+		for($y = 0; $y < $this->_getHeight(); $y++) {
+			for($x = 0; $x < $this->_getWidth(); $x++) {
+				$RGBValue = imagecolorat($this->_imageResource, $x, $y);
+				$colourTable[] = $RGBValue;
+			}
+		}
+		return $colourTable;
     }
+
+    /**
+     * 
+     */
+    private function _createFrequencyTable(array $colourTable) {
+    	$frequencyTable = array();
+    	foreach($colourTable as $colour) {
+    		if(isset($frequencyTable[$colour]))
+    			$frequencyTable[$colour] += 1;
+    		else
+    			$frequencyTable[$colour] = 1;
+    	}
+    	return $frequencyTable;
+    }
+
+    /**
+     * 
+     */
+    private function _applyFrequencyWeights(array $frequencyTable, $threshold = 2) {
+
+    	$weightedFrequencyTable = array();
+    	$discardedColours = array();
+
+    	foreach($frequencyTable as $colour => $frequency) {
+
+    		foreach($frequencyTable as $colourComparison => $frequencyComparison) {
+
+    			//Ignore if we're looking at the current key anyway
+    			if($colour == $colourComparison)
+    				continue;
+
+    			if(in_array($colour, $discardedColours) || in_array($colourComparison, $discardedColours))
+    				continue;
+
+    			$RGB = new RGB($colour);
+    			$RGBComparison = new RGB($colourComparison);
+    			$colourDistance = $RGB->getDistance($RGBComparison);
+
+    			// Check if they are within the threshold. If they are then
+    			// the result with the heigher frequency swollows the lower
+    			// frequency. (om nom nom nom nom)
+    			if($colourDistance < $threshold) {
+    				if($frequency > $frequencyComparison) {
+    					//echo "Keeping {$colour} and discarding {$colourComparison} as distance is {$colourDistance}<br />";
+
+    					$weightedFrequencyTable[$colour] = $frequency + $frequencyComparison;
+    					$discardedColours[] = $colourComparison;
+
+    					//Unset incase it has already been added
+    					unset($weightedFrequencyTable[$colourComparison]);
+    				} else {
+    					//echo "Keeping {$colourComparison} and discarding {$colour} as distance is {$colourDistance}<br />";
+
+    					$weightedFrequencyTable[$colourComparison] = $frequency + $frequencyComparison;
+    					$discardedColours[] = $colour;
+
+    					//Unset incase it has already been added
+    					unset($weightedFrequencyTable[$colour]);
+    				}
+    			} else {
+    				$weightedFrequencyTable[$colour] = $frequency;
+    			}
+    		}
+    	}
+    	return $weightedFrequencyTable;
+    }
+
+    /**
+     * 
+     */
+	private function _getWidth() {
+		return imagesx($this->_imageResource);
+	}
+
+    /**
+     * 
+     */
+	private function _getHeight() {
+		return imagesy($this->_imageResource);
+	}
 
 }
 
@@ -120,8 +227,7 @@ class Create_Colour_Scheme {
 Prototype Usage
 *****************/
 
-$im = imagecreatefromjpeg('img001.jpg');
+//$im = imagecreatefromjpeg('half_black_half_white.jpg');
+$im = imagecreatefromjpeg('half_black_half_white_weight_test.jpg');
 $colour_scheme = new Create_Colour_Scheme($im);
 
-$rgb = new RGB(255, 0, 0);
-echo $rgb;
