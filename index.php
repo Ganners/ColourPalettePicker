513c7ba70803bfbdb0d080ec1befe41aa7009822
Mark Gannaway <mark@ganners.co.uk>
2013-03-07 22:05:48 +0000

    Added new method to create the colour scheme using a while loop

--- index.php
+++ index.php
@@ -63,9 +63,12 @@ class RGB {
 	}
 
 	/**
-	 * 
+	 * Returns the distance from two colours. Using 3D pythagoras
+     * @param [int|RGB] $colour
 	 */
 	public function getDistance($colour) {
+
+        //If the colour isn't currently an RGB object, create one for it
 		if(is_int($colour)) {
 			$colour = new RGB($colour);
 		}
@@ -90,7 +93,6 @@ class RGB {
 		$blue = str_pad(dechex($this->blue), 2, '0');
 		return "#{$red}{$green}{$blue}";
 	}
-
 }
 
 /**
@@ -100,7 +102,7 @@ class RGB {
 class Create_Colour_Scheme {
 
 			/**
-			 * The image resouces, from GD lib
+			 * The image resouces
 			 */
     private $_imageResource,
 			/**
@@ -121,8 +123,34 @@ class Create_Colour_Scheme {
         $this->_imageResource = $image_resource;
         $this->_colourTable = $this->_createColourTable();
         $this->_frequencyTable = $this->_createFrequencyTable($this->_colourTable);
-        $this->_frequencyTable = $this->_applyFrequencyWeights($this->_frequencyTable);
-        var_dump($this->_frequencyTable);
+        
+        /**
+         * Initial solution to the problem. Initially slow and unreliable with optimizations
+         * 
+         * $this->_frequencyTable = $this->_applyFrequencyWeights($this->_frequencyTable);
+         * 
+         */
+
+        /**
+         * New solution, using an array iterator to compare with the next item, reducing the
+         * number of loops significantly.
+         */
+        $this->_frequencyTable = $this->_mergeFrequencyColours($this->_frequencyTable);
+
+        $this->_colourScheme = $this->_convertColourArrayToRGB($this->_frequencyTable);
+    }
+
+    /**
+     * Returns the colour scheme if it's not empty,
+     * else it returns false
+     * 
+     * @return array|false
+     */
+    public function getColourScheme() {
+    	if(!empty($this->_colourScheme))
+    		return $this->_colourScheme;
+    	else
+    		return FALSE;
     }
 
     /**
@@ -142,7 +170,11 @@ class Create_Colour_Scheme {
     }
 
     /**
+     * Creates a frequency table to group same colours
+     * together for faster processing in the next stage
      * 
+     * @param  array $colourTable
+     * @return array
      */
     private function _createFrequencyTable(array $colourTable) {
     	$frequencyTable = array();
@@ -156,12 +188,80 @@ class Create_Colour_Scheme {
     }
 
     /**
+     * 1. Order by RGB
+     * 2. While the count of the array is less than our target frequency
+     *    Loop through each colour
+     * 3. If the colour is in threshold of 1 than the other, merge
+     *    repeat with threshold+1
+     * 4. Order by frequency
      * 
+     * @param array $frequencyTable
+     * @param array $targetTableLength - How long we'd like the table to end up
+     * 
+     * The accuracy is determined by the threshold. The lower the threshold the more loops
+     * and so the slower the calculation.
      */
-    private function _applyFrequencyWeights(array $frequencyTable, $threshold = 2) {
+    private function _mergeFrequencyColours(array $frequencyTable, $targetTableLength = 10) {
+
+        $threshold = 5;
+        $thresholdIncrementations = 5;
+
+        //Order by the RGB key index
+        ksort($frequencyTable);
+
+        //Set table pointer to the beginning
+        reset($frequencyTable);
+
+        while(count($frequencyTable) >= $targetTableLength) {
+            $currentRGB = current($frequencyTable);
+            $currentRGBKey = key($frequencyTable);
+            if(next($frequencyTable)) {
+
+                $nextRGB = current($frequencyTable);
+                $nextRGBKey = key($frequencyTable);
+
+                $RGB1 = new RGB($currentRGBKey);
+                $RGB2 = new RGB($nextRGBKey);
+
+                if($RGB1->getDistance($RGB2) <= $threshold) {
+                    if($currentRGB > $nextRGB) {
+                        //Eat it's frequency value and unset
+                        $frequencyTable[$currentRGBKey] += $frequencyTable[$nextRGBKey];
+                        unset($frequencyTable[$nextRGBKey]);
+                    } else {
+                        //Eat it's frequency value and unset
+                        $frequencyTable[$nextRGBKey] += $frequencyTable[$currentRGBKey];
+                        unset($frequencyTable[$currentRGBKey]);
+                    }
+                }
+            } else {
+                //Add on our new threshold and repeat
+                $threshold += $thresholdIncrementations;
+                reset($frequencyTable);
+            }
+        }
+
+        //Sort it by value
+        arsort($frequencyTable);
+        return $frequencyTable;
+
+    }
+
+    /**
+     * A different solution that compares all items with all items. Problem was it
+     * ran a little slow and returned odd results with larger images. Left in
+     * for reference.
+     */
+    private function _applyFrequencyWeights(array $frequencyTable, $threshold = 8) {
 
     	$weightedFrequencyTable = array();
     	$discardedColours = array();
+        $frequencyLimit = 1000;
+
+        // Need to optimize this loop, though it needs to happen!
+        // Attempt 1. Cut out the lower frequencies, only use the top 100
+        if(count($frequencyTable) > $frequencyLimit)
+            $frequencyTable = array_slice($frequencyTable, 0, $frequencyLimit);
 
     	foreach($frequencyTable as $colour => $frequency) {
 
@@ -183,17 +283,18 @@ class Create_Colour_Scheme {
     			// frequency. (om nom nom nom nom)
     			if($colourDistance < $threshold) {
     				if($frequency > $frequencyComparison) {
-    					//echo "Keeping {$colour} and discarding {$colourComparison} as distance is {$colourDistance}<br />";
 
-    					$weightedFrequencyTable[$colour] = $frequency + $frequencyComparison;
+    					$weightedFrequencyTable[$colour] = $frequency + $frequencyComparison 
+    						+ (isset($weightedFrequencyTable[$colour]) ? $weightedFrequencyTable[$colour] : 0);
+
     					$discardedColours[] = $colourComparison;
 
     					//Unset incase it has already been added
     					unset($weightedFrequencyTable[$colourComparison]);
     				} else {
-    					//echo "Keeping {$colourComparison} and discarding {$colour} as distance is {$colourDistance}<br />";
+    					$weightedFrequencyTable[$colourComparison] = $frequency + $frequencyComparison 
+    						+ (isset($weightedFrequencyTable[$colour]) ? $weightedFrequencyTable[$colour] : 0);
 
-    					$weightedFrequencyTable[$colourComparison] = $frequency + $frequencyComparison;
     					$discardedColours[] = $colour;
 
     					//Unset incase it has already been added
@@ -208,14 +309,29 @@ class Create_Colour_Scheme {
     }
 
     /**
-     * 
+     * Converts our colour frequency table to an array
+     * of RGB objects
+     */
+    private function _convertColourArrayToRGB(array $frequencyTable) {
+
+        $RGBColourTable = array();
+
+    	foreach($frequencyTable as $colour => $frequency) {
+    		$RGBColourTable[$colour] = new RGB($colour);
+    	}
+
+    	return $RGBColourTable;
+    }
+
+    /**
+     * Returns the current image width
      */
 	private function _getWidth() {
 		return imagesx($this->_imageResource);
 	}
 
     /**
-     * 
+     * returns the current image height
      */
 	private function _getHeight() {
 		return imagesy($this->_imageResource);
@@ -227,7 +343,9 @@ class Create_Colour_Scheme {
 Prototype Usage
 *****************/
 
-//$im = imagecreatefromjpeg('half_black_half_white.jpg');
-$im = imagecreatefromjpeg('half_black_half_white_weight_test.jpg');
+$im = imagecreatefromjpeg('412446_486798961348575_2026754479_o.jpg');
 $colour_scheme = new Create_Colour_Scheme($im);
 
+foreach($colour_scheme->getColourScheme() as $colour) { ?>
+	<div style="width: 30px; height: 30px; float: left; background: <?php echo $colour->getHex(); ?>"></div>
+<?php } ?>
